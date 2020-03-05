ba676215766a5c7e6168e28d1e8a03ffbbeda347
morphalex90 <piero.nanni@gmail.com>
2020-03-05 17:47:18 +0000

    start refactoring js

--- script.js
+++ script.js
@@ -26,59 +26,68 @@ jQuery(document).ready(function($){
 });
 
 
-var tempo;
-var centesimi=0;
-var secondi=0;
-var minuti=0;
-var ore=0;
+var time;
+var centesimi = 0;
+var secondi = 0;
+var minuti = 0;
+var ore = 0;
 
 function start(){
 	cronometro();
 }
 
 function pause(){
-	clearTimeout(tempo);
+	clearTimeout(time);
 }
 
 function stop(){
-	clearTimeout(tempo);
-	centesimi=secondi=minuti=ore=0;
+	clearTimeout(time);
+	centesimi = secondi = minuti = ore = 0;
 	mostra();
 }
 
 function mostra(){
-	var tot='';
-	if(ore<10)
-		tot=tot+'0';
-	tot=tot+ore+':';
-	if(minuti<10)
-		tot=tot+'0';
-	tot=tot+minuti+':';
-	if(secondi<10)
-		tot=tot+'0';
-	tot=tot+secondi+':';
-		tot=tot+centesimi;
+	var tot = '';
+
+	if( ore < 10 ) {
+		tot = tot+'0';
+	}
+
+	tot = tot+ore+':';
+
+	if( minuti < 10 ) {
+		tot = tot+'0';
+	}
+
+	tot = tot+minuti+':';
+
+	if( secondi < 10 ) {
+		tot = tot+'0';
+	}
+
+	tot = tot+secondi+':';
+	tot = tot+centesimi;
 	jQuery('.tempo').html(tot);
 }
 
 function cronometro(){
 	
 	centesimi++;
-	if(centesimi==100){
-		centesimi=0;
+	if( centesimi == 100 ){
+		centesimi = 0;
 		secondi++;
 	}
 	
-	if(secondi==60){
-		secondi=0;
+	if( secondi == 60 ){
+		secondi = 0;
 		minuti++;
 	}
 	
-	if(minuti==60){
-		minuti=0;
+	if( minuti == 60 ){
+		minuti = 0;
 		ore++;
 	}
 	
 	mostra();
-	tempo=setTimeout(function(){ cronometro() }, 10);
+	time = setTimeout(function(){ cronometro() }, 10);
 }
