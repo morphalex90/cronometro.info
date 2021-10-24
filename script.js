cc5ed7a05698da2c8f0c8e8a2f25a332eb43a609
morphalex90 <piero.nanni@gmail.com>
2021-10-24 02:49:36 +0100

    indentation command

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 var s = document.getElementsByClassName('loader');
 s = s[0].style;
 s.opacity = 1;
-(function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,40)})();
+(function fade() { (s.opacity -= .1) < 0 ? s.display = "none" : setTimeout(fade, 40) })();
 
 ////////////////////// Click to start / pause
 document.querySelector('.play-pause').addEventListener('click', (e) => {
-	if( document.querySelector('.play-pause').getAttribute('data-mode') == 'play' ) {
+	if (document.querySelector('.play-pause').getAttribute('data-mode') == 'play') {
 		document.querySelector('.play-pause').innerHTML = '<i class="icon-pause2"></i> Pause';
 		document.querySelector('.play-pause').setAttribute('data-mode', 'pause');
 		start();
@@ -28,7 +28,7 @@ document.querySelector('.reset').addEventListener('click', (e) => {
 var lapNumber = 1;
 document.querySelector('.giro').addEventListener('click', (e) => {
 	var lap = document.querySelector('.tempo').textContent;
-	document.querySelector('.partials').insertAdjacentHTML('beforeend', '<tr><td>'+lapNumber+'</td><td>'+lap+'</td></tr>');
+	document.querySelector('.partials').insertAdjacentHTML('beforeend', '<tr><td>' + lapNumber + '</td><td>' + lap + '</td></tr>');
 	lapNumber++;
 });
 
@@ -40,49 +40,49 @@ var secondi = 0;
 var minuti = 0;
 var ore = 0;
 
-function start(){
+function start() {
 	cronometro();
 }
 
-function pause(){
+function pause() {
 	clearTimeout(time);
 }
 
-function stop(){
+function stop() {
 	clearTimeout(time);
 	millesimi = secondi = minuti = ore = 0;
 	show();
 }
 
-function show(){
+function show() {
 	var tot = '';
 
-	tot = tot+String(ore).padStart(2, '0')+':';
-	tot = tot+String(minuti).padStart(2, '0')+':';
-	tot = tot+String(secondi).padStart(2, '0')+':';
-	tot = tot+String(millesimi).padStart(3, '0');
+	tot = tot + String(ore).padStart(2, '0') + ':';
+	tot = tot + String(minuti).padStart(2, '0') + ':';
+	tot = tot + String(secondi).padStart(2, '0') + ':';
+	tot = tot + String(millesimi).padStart(3, '0');
 
 	document.querySelector('.tempo').textContent = tot;
 }
 
-function cronometro(){
-	
+function cronometro() {
+
 	millesimi = millesimi + (4);
-	if( millesimi == 1000 ){
+	if (millesimi == 1000) {
 		millesimi = 0;
 		secondi++;
 	}
-	
-	if( secondi == 60 ){
+
+	if (secondi == 60) {
 		secondi = 0;
 		minuti++;
 	}
-	
-	if( minuti == 60 ){
+
+	if (minuti == 60) {
 		minuti = 0;
 		ore++;
 	}
-	
+
 	show();
-	time = setTimeout(function(){ cronometro() }, 4);
+	time = setTimeout(function () { cronometro() }, 4);
 }
